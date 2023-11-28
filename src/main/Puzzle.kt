d0d1f8bb4a98d9d42655f65e218dea44c7d2a613
christopher schicho <christopher.schicho@gmail.com>
2023-11-28 09:58:02 +0100

    add puzzle interface

--- src/main/Puzzle.kt
+++ src/main/Puzzle.kt
@@ -2,20 +2,31 @@ import java.io.File
 
 interface Puzzle {
 
-    val day: Int
-        get() = this.javaClass.name.takeLast(2).toInt()
+    val day: String
+        get() = this.javaClass.name.takeLast(2)
 
     fun task1(): String
 
     fun task2(): String
 
-    fun readDataFile(readTestFile: Boolean = false): File = File(Utils.getDataPath(day = day, testFile = readTestFile))
+    private fun getInputFilename(readTestFile: Boolean): String = if(readTestFile) "$day-test.txt" else "$day.txt"
 
-    fun printResults() {
+    private fun getInputFilePath(readTestFile: Boolean): String = "data/${getInputFilename(readTestFile)}"
+
+    fun readDataFile(readTestFile: Boolean = false): File = File(getInputFilePath(readTestFile))
+
+    fun printResultTask1() {
         println("### DAY $day / TASK 1 ###")
         println(task1())
+    }
 
+    fun printResultTask2() {
         println("### DAY $day / TASK 2 ###")
         println(task2())
+    }
+
+    fun printResults() {
+        printResultTask1()
+        printResultTask2()
     }
 }
--- src/main/Utils.kt
+++
@@ -1,8 +0,0 @@
-object Utils {
-
-    fun getDataPath(day: Int, testFile: Boolean): String {
-        val dayString = day.toString().padStart(2, '0')
-        val fileName = "${if (testFile) "$dayString-test" else dayString}.txt"
-        return "data/$fileName"
-    }
-}
